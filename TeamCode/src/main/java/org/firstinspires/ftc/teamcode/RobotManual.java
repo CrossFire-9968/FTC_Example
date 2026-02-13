15c0ba036b3a95e4483d725c55d2938715ba1670
Jeff Dawson <dawsonjeffreyd@johndeere.com>
2026-02-12 19:48:39 -0600

    Empty project

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Robot Manual", group = "Manual")
+public class RobotManual extends OpMode
+{
+   public void init() {
+   }
+
+
+   public void loop() {
+   }
+}
